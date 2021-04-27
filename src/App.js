1a4892d0f95d488f6060c6072a0888853de71137
averoli <64157643+averoli@users.noreply.github.com>
2021-04-27 23:39:30 +0200

    Editing App

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import './App.css';
 import Profile from "./Profile";
 import Grid from "@material-ui/core/Grid";
-import {useState} from "react";
-import Form from "./form";
 
 
 function App() {
-
-    const Data = {
-        firstName: 'Olga',
-        secondName: 'Averoli'
-    }
-
-const [profile, setProfile] = useState(Data)
 
     return (
         <Grid container>
             <Grid item xs={12}>
                 <Profile
-                firstName={profile.firstName}
-                secondName={profile.secondName}
                 />
-                <Form />
             </Grid>
         </Grid>
     );
